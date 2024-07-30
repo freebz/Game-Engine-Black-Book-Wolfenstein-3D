0119910157aa8caa9705a475e609269bc2a1c3ed
FreeBz <freebz.dev@gmail.com>
2024-07-30 21:35:23 +0900

    6장 이식

---
+++ ch06/ex6-1.c
@@ -0,0 +1,18 @@
+// 6.3 아이폰
+
+DrawWall( int wallNum ) {
+    char name [128];
+    texture_t *tex;
+    sprintf( name, "walls/%d. tga", wallNum);
+    tex = FindTexture( name );
+...
+}
+texture_t FindTexture( const char *name ) {
+    int   i;
+    for ( i = 0 ;i < numTextures; i++) {
+        if ( !strcmp( name, texture[name]-> name ) ) {
+            return texture[name];
+        }
+    }
+...
+}
---
+++ ch06/ex6-5.c
@@ -0,0 +1,3 @@
+// 6.4 울펜슈타인 3D-VR
+
+boolean virtualreality;
