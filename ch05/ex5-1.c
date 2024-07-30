3f037923ebb5bd7bf29ece7797bfde600dede9ec
FreeBz <freebz.dev@gmail.com>
2024-07-30 20:16:31 +0900

    5장 속편

---
+++ ch05/ex5-1.c
@@ -0,0 +1,13 @@
+// 5.1 운명의 창
+
+BackDoorStrs[5][16] = {
+    "a spoon?",
+    "bite me!",
+    "joshua",
+    "peit",
+#define BETA
+    "beta"
+#else
+    "snoops"
+#endif
+}
---
+++ ch05/ex5-2.c
@@ -0,0 +1,20 @@
+GoodBoyStrs[10][40] = {
+    "...is the CORRECT ANSWER!",
+    "",
+
+    "Consider yourself bitten, sir.",
+    "",
+
+    "Greetings Professor Falken, would you",
+    "like to play Spear of Destiny?",
+
+    "Do you have any gold spray paint?",
+    "",
+
+#ifdef BETA
+    "Beta testing approved.",
+#else
+    "I wish I had a 21\" monitor...",
+#endif
+    ""
+},
